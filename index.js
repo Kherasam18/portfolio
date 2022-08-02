88c1e6e8002807428d080397b3062603a6997cd2
kherasam18 <kherasam18@gmail.com>
2022-08-02 23:48:22 +0530

    portfolio website

---
+++ index.js
@@ -0,0 +1,8 @@
+var text = '{"employees":[' +
+'{"firstName":"John","lastName":"Doe" },' +
+'{"firstName":"Anna","lastName":"Smith" },' +
+'{"firstName":"Peter","lastName":"Jones" }]}';
+
+obj = JSON.parse(text);
+obj.pop();
+console.log(obj)
